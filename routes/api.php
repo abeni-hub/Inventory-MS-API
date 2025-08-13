91fdbb74682428d43ddaaee291deaa53f9eaf900
abeni <abeni@email.com>
2025-08-13 11:52:27 -0700

    add authentication

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ItemController;
 
-Route::apiResource('items', ItemController::class);
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('items', ItemController::class);
+});
