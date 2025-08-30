5c694c728f174cb7f2b218344cdc6a089a61c18e
abeni <abeni@email.com>
2025-08-29 21:00:58 -0700

    add

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SalesOrder;
 
 class Customer extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'email', 'phone', 'address'];
+    public function salesOrders()
+    {
+        return $this->hasMany(SalesOrder::class);
+    }
 }
